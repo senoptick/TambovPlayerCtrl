2edea3b1bfbeee1273f4eb0ab9959701d62e55bf
senoptick <113616549+senoptick@users.noreply.github.com>
2026-03-05 22:04:17 +0300

    Update TambovPlayerCtrl.py

--- TambovPlayerCtrl.py
+++ TambovPlayerCtrl.py
@@ -1,8 +1,26 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-FileCopyrightText: 2023 Kent Gibson <warthog618@gmail.com>
+
+"""Minimal example of reading a single line."""
+
 import gpiod
 
-chip = gpiod.Chip('gpiochip0')
-line = chip.get_line(6)
+from gpiod.line import Direction
 
-line.request(consumer="test", type=gpiod.LINE_REQ_DIR_IN)
 
-print("value:", line.get_value())
+def get_line_value(chip_path, line_offset):
+    with gpiod.request_lines(
+        chip_path,
+        consumer="get-line-value",
+        config={line_offset: gpiod.LineSettings(direction=Direction.INPUT)},
+    ) as request:
+        value = request.get_value(line_offset)
+        print("{}={}".format(line_offset, value))
+
+
+if __name__ == "__main__":
+    try:
+        get_line_value("/dev/gpiochip0", 5)
+    except OSError as ex:
+        print(ex, "\nCustomise the example configuration to suit your situation")
